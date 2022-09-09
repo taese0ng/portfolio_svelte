f3147652425936c06a1c8731c7702a1bb695f698
taese0ng <taese0ng@naver.com>
2022-09-09 17:14:37 +0900

    feat: open mobile mode Skill page

--- src/pages/Mobile/Skill.svelte
+++ src/pages/Mobile/Skill.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
 	import Layout from "@components/Mobile/Layout";
+	import { skillList } from "@constants/skills";
 </script>
 
 <Layout>
-	<div>skill</div>
+	<ul class="skills">
+		{#each skillList as skill}
+			<li class="skills__item">
+				<img class="skills__item--img" src="{skill.src}" alt="{skill.title}" />
+			</li>
+		{/each}
+	</ul>
 </Layout>
 
 <style src="./Skill.scss"></style>
